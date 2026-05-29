794850ecbc97eff625c680caa10aa22b7e83ba8d
Jared Ledbetter <jared@carbondigital.us>
2026-05-28 23:54:02 -0400

    setup topbar notice

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,9 @@ import Header from '../components/organisms/Header';
 export default function Home() {
     return (
         <>
+            <div className='w-full bg-red-900 text-stone-100 font-bold text-lg text-center uppercase py-2'>
+                Notice: This station is not on the airwaves yet.
+            </div>
             <Header />
             <section>
                 <h1>Date Section</h1>
