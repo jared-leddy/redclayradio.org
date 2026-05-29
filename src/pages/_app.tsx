7313884d9d3cf1217f05d0d937527ea4865d9863
Jared Ledbetter <jared@carbondigital.us>
2026-05-28 22:12:52 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import "@/src/styles/globals.css";
-import type { AppProps } from "next/app";
+// NPM Modules
+import type { AppProps } from 'next/app';
+
+// Custom Modules
+import '@/src/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
