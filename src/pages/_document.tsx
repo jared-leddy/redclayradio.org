e040d31e92335f6588313c31e95bf613120a7a04
Jared Ledbetter <jared@carbondigital.us>
2026-05-28 23:15:48 -0400

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     return (
         <Html lang='en'>
             <Head />
-            <body className='antialiased'>
+            <body className='antialiased bg-zinc-950'>
                 <Main />
                 <NextScript />
             </body>
