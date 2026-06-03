179f6bf1e922eaf53fe120286b0713638dce9d37
Jared Ledbetter <jared@carbondigital.us>
2026-06-02 21:54:28 -0400

    Update future.tsx

--- src/pages/future.tsx
+++ src/pages/future.tsx
@@ -1,10 +1,26 @@
 // Custom Modules
 import StageSection from '@/components/organisms/StageSection';
+import type OnDeckListItem from '@/utils/interfaces/OnDeckListItem.interface';
 import OnDeckBanner from '../components/molecules/OnDeckBanner';
 import SongBanner from '../components/molecules/SongBanner';
 import DateSection from '../components/organisms/DateSection';
 import Footer from '../components/organisms/Footer';
 import Header from '../components/organisms/Header';
+
+const onDeck: OnDeckListItem[] = [
+    { artist: 'Knocked Loose', genre: 'Metalcore' },
+    { artist: 'Turnstile', genre: 'Hardcore' },
+    { artist: 'Chelsea Wolfe', genre: 'Darkwave' },
+    { artist: 'Loathe', genre: 'Alternative Metal' },
+    { artist: 'JPEGMAFIA', genre: 'Experimental Hip-Hop' },
+    { artist: 'VOLA', genre: 'Progressive Metal' },
+    { artist: 'HEALTH', genre: 'Industrial' },
+    { artist: 'Bad Omens', genre: 'Alternative Metal' },
+    { artist: 'JPEGMAFIA', genre: 'Experimental Hip-Hop' },
+    { artist: 'VOLA', genre: 'Progressive Metal' },
+    { artist: 'HEALTH', genre: 'Industrial' },
+    { artist: 'Bad Omens', genre: 'Alternative Metal' },
+];
 
 export default function FutureHome() {
     return (
@@ -16,7 +32,7 @@ export default function FutureHome() {
             <DateSection />
             <StageSection />
             <SongBanner />
-            <OnDeckBanner />
+            <OnDeckBanner items={onDeck} />
             <Footer />
         </>
     );
