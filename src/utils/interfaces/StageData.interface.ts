b373185dda70dcc75597015495d30b9862df46a8
Jared Ledbetter <jared@carbondigital.us>
2026-06-03 19:44:03 -0400

    drop player type usage

--- src/utils/interfaces/StageData.interface.ts
+++ src/utils/interfaces/StageData.interface.ts
@@ -1,12 +1,10 @@
 // Custom Modules
-import type { PlayerType } from '../enums/PlayerType.enum';
 import type { StageType } from '../enums/StageType.enum';
 
 export default interface StageData {
     artist: string;
     genres: string[];
     location: string;
-    playerType: PlayerType;
     playerURL: string;
     stageType: StageType;
 }
