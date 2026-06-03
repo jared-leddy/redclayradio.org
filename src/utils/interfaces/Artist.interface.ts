8ad7a39cfe48b745d93a9eaea460972a572532b7
Jared Ledbetter <jared@carbondigital.us>
2026-06-03 19:43:58 -0400

    update to extend create artist

--- src/utils/interfaces/Artist.interface.ts
+++ src/utils/interfaces/Artist.interface.ts
@@ -1,13 +1,8 @@
 // Custom Modules
 import type { ArtistStatus } from '../enums/ArtistStatus.enum';
-import type { PlayerType } from '../enums/PlayerType.enum';
+import type ArtistCreate from './ArtistCreate.interface';
 
-export default interface Artist {
-    genres: string[];
+export default interface Artist extends ArtistCreate {
     isActive: boolean;
-    location: string;
-    name: string;
-    playerType: PlayerType;
-    playerURL: string;
     status: ArtistStatus;
 }
