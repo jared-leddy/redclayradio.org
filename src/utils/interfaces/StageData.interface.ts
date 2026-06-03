54eb2dfd4674d97cf4bc7f9b04f0ca5d2540df31
Jared Ledbetter <jared@carbondigital.us>
2026-06-02 21:54:29 -0400

    Update StageData.interface.ts

--- src/utils/interfaces/StageData.interface.ts
+++ src/utils/interfaces/StageData.interface.ts
@@ -1,6 +1,6 @@
 // Custom Modules
-import { PlayerType } from '../enums/PlayerType.enum';
-import { StageType } from '../enums/StageType.enum';
+import type { PlayerType } from '../enums/PlayerType.enum';
+import type { StageType } from '../enums/StageType.enum';
 
 export default interface StageData {
     artist: string;
