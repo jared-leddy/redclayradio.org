f9170467de57413a90fa71d9213aa5c35dabbd60
Jared Ledbetter <jared@carbondigital.us>
2026-06-03 00:32:21 -0400

    Update Artist.interface.ts

--- src/utils/interfaces/Artist.interface.ts
+++ src/utils/interfaces/Artist.interface.ts
@@ -1,10 +1,13 @@
 // Custom Modules
+import type { ArtistStatus } from '../enums/ArtistStatus.enum';
 import type { PlayerType } from '../enums/PlayerType.enum';
 
 export default interface Artist {
     genres: string[];
+    isActive: boolean;
     location: string;
     name: string;
     playerType: PlayerType;
     playerURL: string;
+    status: ArtistStatus;
 }
