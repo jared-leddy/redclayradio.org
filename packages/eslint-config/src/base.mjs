148c28d8e7bb1f1ec63952eb50d011a4e2e20cd2
Jared Ledbetter <jared@carbondigital.us>
2026-06-03 22:13:50 -0400

    Create base.mjs

---
+++ packages/eslint-config/src/base.mjs
@@ -0,0 +1,81 @@
+// NPM Modules
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import onlyWarn from 'eslint-plugin-only-warn';
+import turboPlugin from 'eslint-plugin-turbo';
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+/**
+ * A shared ESLint configuration for the repository.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ */
+export const config = [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      turbo: turboPlugin,
+      prettier: prettierPlugin
+    },
+    rules: {
+      'turbo/no-undeclared-env-vars': 'warn',
+      'prettier/prettier': 'error',
+
+      // Custom TS rules
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_'
+        }
+      ]
+    }
+  },
+  {
+    plugins: {
+      onlyWarn
+    }
+  },
+  {
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'coverage/**',
+      '**/*.config.js',
+      '**/*.config.mjs',
+      '**/*.config.ts',
+      'eslint.config.mjs'
+    ]
+  },
+  // TypeScript files
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        sourceType: 'module'
+      }
+    }
+  },
+  // JavaScript files
+  {
+    files: ['**/*.js', '**/*.mjs'],
+    languageOptions: {
+      sourceType: 'module'
+    }
+  }
+];
