8071c5a6a6a8d194cfa97318c6345629a7839887
Jared Ledbetter <jared@carbondigital.us>
2026-06-03 22:02:31 -0400

    Delete Artist.interface.ts

--- src/utils/interfaces/Artist.interface.ts
+++
@@ -1,8 +0,0 @@
-// Custom Modules
-import type { ArtistStatus } from '../enums/ArtistStatus.enum';
-import type ArtistCreate from './ArtistCreate.interface';
-
-export default interface Artist extends ArtistCreate {
-    isActive: boolean;
-    status: ArtistStatus;
-}
