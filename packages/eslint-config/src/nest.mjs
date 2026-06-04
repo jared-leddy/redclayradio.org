e0b8524803ddffc62ae9f3324601afd40b605fd2
Jared Ledbetter <jared@carbondigital.us>
2026-06-03 22:13:39 -0400

    Create nest.mjs

---
+++ packages/eslint-config/src/nest.mjs
@@ -0,0 +1,33 @@
+// NPM Modules
+import globals from 'globals';
+
+// Custom Modules
+import { config as baseConfig } from './base.mjs';
+
+/**
+ * A custom ESLint configuration for Nest.js.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ */
+export const nestJSConfig = [
+  ...baseConfig,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true
+      }
+    }
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn'
+    }
+  }
+];
