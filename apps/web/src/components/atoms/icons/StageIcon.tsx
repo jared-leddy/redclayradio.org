1b365498f0419a6cdee1e53d08518ef7dbd4bea6
Jared Ledbetter <jared@carbondigital.us>
2026-06-03 22:26:32 -0400

    Create StageIcon.tsx

---
+++ apps/web/src/components/atoms/icons/StageIcon.tsx
@@ -0,0 +1,7 @@
+export default function StageIcon() {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 640 640' className='h-6 w-6 fill-red-800'>
+      <path d='M320 64L48 256L48 560L208 560L208 384L432 384L432 560L592 560L592 256Z' />
+    </svg>
+  );
+}
