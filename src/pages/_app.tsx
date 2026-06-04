499dd73592048215478cb8c2e1c3950a21358732
Jared Ledbetter <jared@carbondigital.us>
2026-06-03 22:02:17 -0400

    Delete _app.tsx

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-// NPM Modules
-import type { AppProps } from 'next/app';
-
-// Custom Modules
-import '@/assets/css/globals.css';
-
-export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
-}
