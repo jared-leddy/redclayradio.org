790c5e96709dd3011eaa74c0363cae98cf46c6bb
Jared Ledbetter <jared@carbondigital.us>
2026-06-05 02:08:11 -0400

    Update index.ts

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -6,4 +6,6 @@ export { StageType } from './enums/StageType.enum';
 export type { default as Artist } from './interfaces/Artist.interface';
 export type { default as ArtistCreate } from './interfaces/ArtistCreate.interface';
 export type { default as OnDeckListItem } from './interfaces/OnDeckListItem.interface';
+export type { default as RawSpotifyArtist } from './interfaces/RawSpotifyArtist.interface';
 export type { default as SpotifyArtist } from './interfaces/SpotifyArtist.interface';
+export type { default as SpotifyTokenResponse } from './interfaces/SpotifyTokenResponse.interface';
