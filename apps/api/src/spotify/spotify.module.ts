f6ac554afe78919ede82d6ba3a067a225c0a943a
Jared Ledbetter <jared@carbondigital.us>
2026-06-07 14:32:30 -0400

    Update spotify.module.ts

--- apps/api/src/spotify/spotify.module.ts
+++ apps/api/src/spotify/spotify.module.ts
@@ -2,12 +2,10 @@
 import { Module } from '@nestjs/common';
 
 // Custom Modules
-import ArtistModule from '@/artist/artist.module';
 import SpotifyController from './spotify.controller';
 import SpotifyService from './spotify.service';
 
 @Module({
-  imports: [ArtistModule],
   controllers: [SpotifyController],
   providers: [SpotifyService],
   exports: [SpotifyService]
