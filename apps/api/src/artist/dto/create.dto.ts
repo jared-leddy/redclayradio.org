327a4db58c504dfa838b5734f7792976642028ba
Jared Ledbetter <jared@carbondigital.us>
2026-06-07 13:12:16 -0400

    Update create.dto.ts

--- apps/api/src/artist/dto/create.dto.ts
+++ apps/api/src/artist/dto/create.dto.ts
@@ -1,23 +1,14 @@
 // NPM Modules
-import { ArrayMaxSize, IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export default class ArtistCreateDTO {
   @IsString()
   @IsNotEmpty()
   name: string;
 
-  @IsArray()
-  @ArrayMaxSize(20)
-  @IsString({ each: true })
-  genres: string[];
-
   @IsString()
   @IsOptional()
   playerURL?: string;
-
-  @IsString()
-  @IsOptional()
-  location?: string;
 
   @IsString()
   @IsNotEmpty()
