a103e8afb5ab5fc48e1ee0ca594001c345bee6d2
Jared Ledbetter <jared@carbondigital.us>
2026-06-07 20:25:56 -0400

    Update artist.module.ts

--- apps/api/src/artist/artist.module.ts
+++ apps/api/src/artist/artist.module.ts
@@ -4,11 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 // Custom Modules
 import Artist from '@/database/artist.entity';
+import GenreModule from '@/genre/genre.module';
+import MusicBrainzModule from '@/musicbrainz/musicbrainz.module';
 import ArtistController from './artist.controller';
 import ArtistService from './artist.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Artist])],
+  imports: [TypeOrmModule.forFeature([Artist]), GenreModule, MusicBrainzModule],
   controllers: [ArtistController],
   providers: [ArtistService],
   exports: [ArtistService]
